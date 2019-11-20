830f832ef67426ddcf597a81c9d4c4c4ebc03a19
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2019-11-20 21:50:12 +0900

    drag(2)

--- Project_jk/Assets/Scripts/WaitingTable.cs
+++ Project_jk/Assets/Scripts/WaitingTable.cs
@@ -4,34 +4,7 @@ using UnityEngine;
 
 public class WaitingTable : MonoBehaviour
 {
-    private bool IsWaiting;
+    
 
-    private void Awake()
-    {
-        IsWaiting = true;
-    }
-
-    public bool WaitingPossible()
-    {
-        return IsWaiting;
-    }
-
-    public void ChangeWaiting()
-    {
-        IsWaiting = !IsWaiting;
-    }
-
-    private void OnMouseUp()
-    {
-        if(Input.GetMouseButtonUp(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.Log(hit.transform.gameObject.name);
-            }
-        }
-    }
+    
 }
