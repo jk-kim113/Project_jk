f0186d2a53b5bf1ea184621514f48c80fb68ca76
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2019-11-21 15:29:48 +0900

    drag(3)

--- Project_jk/Assets/Scripts/BattleTable.cs
+++ Project_jk/Assets/Scripts/BattleTable.cs
@@ -10,20 +10,4 @@ public class BattleTable : MonoBehaviour
     {
         mPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
-
-    private void OnMouseUp()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-        {
-            Debug.Log(hit.collider.gameObject.name);
-            if (hit.collider.gameObject.CompareTag("Monster"))
-            {
-                Debug.Log(hit.collider.gameObject.name);
-                mPlayer.transform.position = hit.collider.gameObject.transform.position;
-            }
-
-        }
-    }
 }
--- Project_jk/Assets/Scripts/PlayerController.cs
+++ Project_jk/Assets/Scripts/PlayerController.cs
@@ -7,34 +7,47 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     protected float mATK, mDEF, mHEAL;
 
+    private Transform mBattletablePos;
+    private Transform mStartPos;
+
     private void Start()
     {
-    
+        mStartPos = transform;
     }
 
     private void Update()
     {
-        
+        RaycastHit hit;
+        Vector3 mPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(mPos), out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.CompareTag("Monster"))
+            {
+                mBattletablePos = hit.collider.transform;
+            }
+            else
+            {
+                mBattletablePos = null;
+            }
+            Debug.Log(mBattletablePos);
+        }
     }
 
     private void OnMouseDrag()
     {
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 15);
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 40);
     }
 
-    //private void OnMouseUp()
-    //{
-    //    RaycastHit hit;
-
-    //    if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-    //    {
-    //        Debug.Log(hit.collider.gameObject.name);
-    //        if (hit.collider.gameObject.CompareTag("Player"))
-    //        {
-    //            Debug.Log(hit.collider.gameObject.name);
-    //            transform.position = hit.collider.gameObject.transform.position;
-    //        }
-            
-    //    }
-    //}
+    private void OnMouseUp()
+    {
+        if(mBattletablePos == null)
+        {
+            transform.position = mStartPos.position;
+        }
+        else
+        {
+            transform.position = mBattletablePos.position;
+        }
+    }
 }
